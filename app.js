b8c3a3f12288fdac430837f48afff3aaa99056bd
RockyBaura <251drougnov@gmail.com>
2023-02-11 22:34:26 +0600

    Url fetched

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const toggleMenu = document.querySelector('.nav__menu');
 toggleMenu.addEventListener('click',()=>{
     nav.classList.toggle('active');
 })
+
+const urlForm = document.querySelector('.url__form');
+const urlInput = document.querySelector('.url__input');
+const urlSubmit = document.querySelector('.url__submit');
+
+urlForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    fetch(`https://api.shrtco.de/v2/shorten?url=${urlInput.value}`)
+    .then(res => res.json())
+    .then(data=> console.log(data))
+    .catch(error => console.log(error))
+})
