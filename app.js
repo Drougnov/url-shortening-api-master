d1ccbc5937d08448f47caa2176b47ee0dee9b561
RockyBaura <251drougnov@gmail.com>
2023-03-27 22:38:06 +0600

    Refactor: tab width

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const nav = document.querySelector('nav');
-const toggleMenu = document.querySelector('.nav__menu');
+const nav = document.querySelector("nav");
+const toggleMenu = document.querySelector(".nav__menu");
+const urlError = document.querySelector(".url__error");
 
-toggleMenu.addEventListener('click',()=>{
-    nav.classList.toggle('active');
-})
+toggleMenu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
 
-const urlForm = document.querySelector('.url__form');
-const urlInput = document.querySelector('.url__input');
-const urlSubmit = document.querySelector('.url__submit');
+const urlForm = document.querySelector(".url__form");
+const urlInput = document.querySelector(".url__input");
+const urlSubmit = document.querySelector(".url__submit");
 
-urlForm.addEventListener('submit',(e)=>{
+urlForm.addEventListener("submit", (e) => {
     e.preventDefault();
     fetch(`https://api.shrtco.de/v2/shorten?url=${urlInput.value}`)
-    .then(res => res.json())
-    .then(data=> console.log(data))
-    .catch(error => console.log(error))
-})
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            if (!data.ok) {
+                urlError.textContent = data.error;
+            }
+        });
+});
